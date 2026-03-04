eec7bd8c22c3b0921217997f1371e9cd87c3d214
alvaro <alvarolassaletta@gmail.com>
2026-03-04 13:39:30 +0100

    feat: updated autentificacion service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,45 +1,62 @@
 import 'package:proyecto_intermodular/supabase_config.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-
-
 class AuthService{
   final  _supabase = SupabaseConfig.client; 
 
-  /*REGISTRAR USUARIO - SIGN UP 
-  Método para registrar un usuario  con  email y contraseña  usando el método auth.signUp
-  Supabase enviía un correo de confirmación  que el usuario debe verificar 
-  Devulve un Oobjeto AuthResponse que usaremos para obtener información del usuario y de la sesion
-  Usamos la propiedad data para pasar  nombre de usuario y nombre completo.
-  Cunado se activa este método, se activara un trigger  que llamará a un método para hacer la inserción del usuario en 
-  la tabla profiles*/ 
+  ///REGISTRAR USUARIO - SIGN UP 
+  ///Método para registrar un usuario  con  email y contraseña  usando el método auth.signUp
+  ///Supabase enviía un correo de confirmación  que el usuario debe verificar 
+  ///Devulve un Objeto AuthResponse que usaremos para obtener información del usuario y de la sesion
+  ///Usamos la propiedad data para pasar  nombre de usuario y nombre completo.
+  ///Cunado se activa este método, se activara un trigger  que llamará a un método para hacer la inserción del usuario en la tabla profiles
   Future <AuthResponse> signUp({
      required String email,
      required String password, 
      required String userName,
-     required fullName}) async {
-     return await _supabase.auth.signUp(
-      email:email,
-      password: password,
-      data: {
-        'user_name': userName,
-        'full_name': fullName,
-        }
-      );
+     required String fullName}) async {
+      try{
+        return await _supabase.auth.signUp(
+          email:email,
+          password: password,
+          data: {
+            'user_name': userName,
+            'full_name': fullName,
+          }
+        );
+      } on AuthException catch(e){
+        throw Exception('Error de registro: ${e.message}');
+      } catch(e){
+        throw Exception('Error inesperado al registrarse $e}');
+      }
+     
   }
   
-  /*  INICIAR SESION - SIGN IN 
-  Método para que  un usuario existente en la tabla  interna de supabase auth.users inicie sesion  con email y contraseña utilizando  auth.signInWithPassword
-  */ 
+  ///  INICIAR SESION - SIGN IN 
+  ///Método para que  un usuario existente en la tabla  interna de supabase auth.users inicie sesion  con email y contraseña utilizando  auth.signInWithPassword
+  
   Future <AuthResponse> signIn({required String email, required  String password}) async {
-     return await  _supabase.auth.signInWithPassword(email: email, password: password); 
+    try{
+      return await  _supabase.auth.signInWithPassword(email: email, password: password); 
+    } on AuthException catch(e){
+      throw Exception('Error al iniciar sesion ${e.message}');
+    } catch(e){
+      throw Exception('Error inesperado al iniciar sesion: $e');
+    }
+     
   }
 
   //CERRAR SESIÓN - SIGN OUT 
   //Cierra la sesión del usuario , si  tiene una sesión abierta 
   //requiere que el usuario  haya iniciado previamente sesión 
   Future <void>  signOut () async {
-    return await  _supabase.auth.signOut();
+    try{
+      return await  _supabase.auth.signOut();
+    } on AuthException catch(e){
+      throw  Exception('Error al cerrar sesión ${e.message}');
+    } catch(e){
+      throw Exception('Error inesperado al cerrar sesión: $e');
+    }
   }
 
   //getter  para objener el usuario actual 
