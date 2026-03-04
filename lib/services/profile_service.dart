75239dbdd40e442716e17656b8e8d41b472a2ba6
alvaro <alvarolassaletta@gmail.com>
2026-03-04 13:38:31 +0100

    feat: add profile service && time_entry service

---
+++ lib/services/profile_service.dart
@@ -0,0 +1,90 @@
+import '../models/profiles.dart'; 
+import '../supabase_config.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+
+class ProfileService{
+
+  ///recuperamos el cliente de SupabaseConfig 
+   final SupabaseClient _supabase = SupabaseConfig.client; 
+
+   /// SELECT 
+   ///Busca el profiles por el id . Si lo encuentra devuelve un objeto Profile. En caso contrario devuelve null 
+
+  Future <Profile?> getProfile( String id) async{
+    try{
+      final data =  await _supabase.from('profiles')
+      .select()
+      .eq('id',id)
+      .maybeSingle();
+
+      return  data != null ? Profile.fromMap(data) : null; 
+    }on PostgrestException catch(e){
+      print('Error DB : ${e.message} , codigo: ${e.code}');
+      return null; 
+    }catch(e){
+      print('Error: $e'); 
+      return null; 
+    }
+  }
+    
+   
+   ///SELECT ALL
+   /// Método pensado para que el  usuario que sea administrador pueda ver todos los perfiles 
+  
+   Future <List<Profile>> getProfiles() async{
+    try{
+      final data =  await _supabase.from('profiles').select();
+      return data.map((profileMap)=> Profile.fromMap(profileMap)).toList(); 
+
+    } on PostgrestException catch(e){
+      print('Error DB : ${e.message} , codigo: ${e.code}');
+      return []; 
+    }catch(e){
+      print('Error: $e'); 
+      return []; 
+    }
+  }
+
+
+   /// UPDATE 
+   ///el id es obligatorio y el nombre de usuario y el nobmre completo seran opcionales. El usuario puede querer modifcar uno u otro o ambos. 
+   /// hay que comprobar que los datos del formulario no vengan vacios o con valor null para evitar  que al hacer el update se inserte null o vacio en la fila 
+
+  Future <void> updateProfile( {required String id, String? userName, String? fullName})async {
+    ///update toma un mapa 
+    final Map<String,dynamic> updates = {}; 
+
+    ///Solo cuando el usuario haya introducido un nombre de usuario, actualizaremos el campo
+    if(userName!= null &&  userName.isNotEmpty){
+      updates['user_name']=userName; 
+    }
+    ///Solo cuando el usuario haya introducido un nombre, actualizaremos el campo
+    if(fullName !=null && fullName.isNotEmpty){
+      updates['full_name'] = fullName;
+    }
+
+    if(updates.isNotEmpty){
+      try{
+        await _supabase.from('profiles')
+          .update(updates)
+          .eq('id',id);
+      } on PostgrestException catch(e){
+         throw Exception('Error al actualizar el perfil: ${e.message} , codigo: ${e.code}');
+      }catch(e){
+        throw Exception('Error inesperado al actulizar el perfil');
+      }
+       
+    }
+  }
+
+
+   /// INSERT -> No configuramos una consulta insert porque  la creación del perfil  se realiza 
+   /// mediante una función postgres y un trigger. Cuando  ocurre el signup y se inserta un profile en la tabla interna de supabase
+   /// auth.users() , se activa un trigger que llama a la función  que realiza la insercción en nuestra tabla profiles 
+
+   /// DELETE -> No interesa que un usuario pueda borrar su perfil. De hecho, ni siquiera se ha  creado la policy en supabase 
+
+
+
+}
---
+++ lib/services/time_entry_service.dart
@@ -0,0 +1,93 @@
+import '../supabase_config.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../models/time_entries.dart';
+
+
+class TimeEntryService{
+
+  ///recuperamos el cliente de SupabaseConfig 
+  final SupabaseClient _supabase =  SupabaseConfig.client; 
+
+  ///  INSERT - FICHAJE ENTRADA 
+  /// Solo pasamos el usuario que realiza el fichaje. El campo para el fichaje de entrada (clock_in) tiene un valor por defecto NOW() y el de salida queda en null de momento
+  //en vez de hacer INSERT y SELECT del fichaje de entrada  en dos consultas separadas, se puede hacer en una 
+  /// Devuelve objeto timeEntry si existe. Si no, devuelve null
+
+  Future <TimeEntry?> clockIn(String userId) async {
+    try{
+      final data =  await _supabase.from('time_entries')
+        .insert({'user_id':userId})
+        .select()
+        .maybeSingle();
+      return data != null ? TimeEntry.fromMap(data) : null; 
+    } on PostgrestException catch(e){
+      throw Exception('Error DB: ${e.message} , codigo: ${e.code}');
+    }catch(e){
+      throw Exception('Error $e'); 
+    }
+  }
+
+  ///UPDATE - FICHAJE SALIDA 
+  ///clock_out habia quedado en null. Ahora se guarda  el fichade de salida emn UTC y en un formato compatible con el tipo de dato del campo (TIMESTAMP WITH TIME ZONE)
+
+  Future<void> clockOut(String timeEntryId) async{
+    try{
+      final clockOut = DateTime.now().toUtc().toIso8601String();
+      await _supabase.from('time_entries')
+        .update({'clock_out': clockOut})
+        .eq('id',timeEntryId);
+
+    } on PostgrestException catch(e){
+      throw Exception('Error DB: ${e.message} , codigo: ${e.code}');
+    }catch(e){
+      throw Exception('Error $e'); 
+    }
+  }
+
+  /// SELECT - COMPROBAR SI HAY FICHAJE ACTIVO
+  ///Buscamos el registro en el que el campo clock_out es null
+  ///Esto es, el fichaje en el que solo se ha realizado la entrada y no la salida
+  ///Devuelve objeto TimeEntry si existe. Si no , devulve null
+
+  Future <TimeEntry?>  getActiveTimeEntry(String userId) async{
+    try{
+      final data = await _supabase.from('time_entries')
+        .select()
+        .eq('user_id', userId)
+        .isFilter('clock_out', null)
+        .maybeSingle();
+      return data != null ? TimeEntry.fromMap(data) : null; 
+
+    } on PostgrestException catch(e){
+      print('Error DB: ${e.message} , codigo: ${e.code}');
+      return null; 
+    }catch(e){
+      print('Error $e'); 
+      return null; 
+    }
+  }
+
+  /// SELECT ALL TIME-ENTRIES 
+  /// El usuario puede ver sus registros
+  /// el administrador puede ver todos los registros 
+  Future <List<TimeEntry>>  getAllTimeEntries()async {
+    try{
+      final data = await _supabase.from('time_entries')
+        .select()
+        .order('created_at',ascending:false); 
+      return data.map((timeEntryMap)=> TimeEntry.fromMap(timeEntryMap) ).toList(); 
+
+    }on PostgrestException catch(e){
+      print('Error DB: ${e.message} , codigo: ${e.code}');
+      return []; 
+    }catch(e){
+      print('Error $e'); 
+      return []; 
+    }
+  }
+  
+  ///DELETE 
+  ///No interesa que el usuario pueda eliminar  sus fichajes
+  ///Nuevamente, tampoco se ha creado una politica  para DELETE
+
+}
