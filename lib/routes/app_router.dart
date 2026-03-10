131731725538539c230daa7cf20ea308c26fe89d
alvaro <alvarolassaletta@gmail.com>
2026-03-10 14:25:51 +0100

    feat: add router file  for navigation based on go router package

---
+++ lib/routes/app_router.dart
@@ -0,0 +1,71 @@
+import 'package:go_router/go_router.dart';
+import '../screens/home_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/signup_screen.dart';
+import '../screens/splash_screen.dart';
+
+///navegacion anidada dentro de homeScreen.
+//navegacion del menu NavigationBar
+import  '../screens/clock_screen.dart';
+import '../screens/summary_screen.dart';
+import '../screens/profile_screen.dart';
+import '../screens/time_entry_record_screen.dart'; 
+
+final GoRouter appRouter = GoRouter(
+  initialLocation:  '/splash',
+  routes: [
+    GoRoute(
+      path:'/splash',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path:'/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path:'/signup',
+      builder: (context, state) => const SignUpScreen(),
+    ),
+
+    StatefulShellRoute.indexedStack(
+      builder: (context,state,navigationShell){
+        return HomeScreen(navigationShell: navigationShell);
+      },
+      branches: [
+        StatefulShellBranch(
+          routes:[
+            GoRoute(
+              path:'/home', // cuando haga la ClockScreen me tengo que acordar de  incluirla aqui
+              builder: (context,state) => const ClockScreen(),
+            )
+          ]
+        ),
+        StatefulShellBranch(
+          routes:[
+            GoRoute(
+              path:'/time_entry_record', 
+              builder: (context,state) => const TimeEntryRecordScreen(),
+            )
+          ]
+        ),
+        StatefulShellBranch(
+          routes:[
+            GoRoute(
+              path:'/summary', 
+              builder: (context,state) => SummaryScreen(),
+            )
+          ]
+        ),
+        StatefulShellBranch(
+          routes:[
+            GoRoute(
+              path:'/profile', 
+              builder: (context,state) => const ProfileScreen(),
+            )
+          ]
+        ),
+      ]
+    )
+  ]
+);
+
