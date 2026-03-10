5c3f2ae1ab54abdc0530dbf89120cee80abeac93
alvaro <alvarolassaletta@gmail.com>
2026-03-10 02:00:46 +0100

    feat: add keys for flutter secure storage

---
+++ lib/utils/preferences_keys.dart
@@ -0,0 +1,9 @@
+class PreferencesKeys {
+
+  ///Flutter secure storage 
+  ///El objetivo es que se recuerda el email y la contraseña del usuario  en los CustomTextFormField 
+  /// flutter secure storage guarda  como String 
+  static const String rememberCredentials = "remember_credential"; 
+  static const String savedEmail = "saved_email";
+  static const String savedPassword = "saved_password"; 
+}
