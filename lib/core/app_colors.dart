a0de988ae25fdaaaa1c99ba5c23d5c5eca862812
alvaro <alvarolassaletta@gmail.com>
2026-03-15 22:06:18 +0100

    feat: add app_colors file

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors{
+
+  ///Colores de fondo para component GradientBackground
+  static const Color gradientBackgroundStart = Color(0xCA9EEAB3);
+  static const Color gradientBackgroundMiddle = Color(0xFF17DB93);
+  static const Color gradientBackgroundEnd=   Color(0xFF00D3CC);
+
+
+}
