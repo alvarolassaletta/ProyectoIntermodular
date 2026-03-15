c4ed504162a72d5d0fa907c92bfaa5871ad0f185
alvaro <alvarolassaletta@gmail.com>
2026-03-15 22:15:10 +0100

    refactor: update profile model

--- lib/models/profiles.dart
+++ lib/models/profiles.dart
@@ -26,17 +26,6 @@ class Profile{
     );
   }
   
-  /* vamos a necetiar algo asi en el futuro  si se quiere actualziar el perfil 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'user_name': userName,
-      'full_name': fullName,
-      // Convertimos la fecha de Dart a un texto que Postgres entienda
-      'created_at': createdAt.toIso8601String(), 
-      'is_admin': isAdmin,
-    };
-  }
-  */
+
 
 }
